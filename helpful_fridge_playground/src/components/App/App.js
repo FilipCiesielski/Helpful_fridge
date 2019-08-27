ddbfcb88af6dc4700f6b652ae3eb09bb79434dbc
FilipCiesielski <filip.carpenter@gmail.com>
2019-08-27 16:52:05 +0200

    firstStep

---
+++ helpful_fridge_playground/src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import FridgeBox from "../Box/Box";
+
+function App() {
+  return (
+  <FridgeBox/>
+  );
+}
+
+export default App;
---
+++ helpful_fridge_playground/src/components/Box/Box.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./Box.scss"
+
+class FridgeBox extends Component{
+    render() {
+        return (
+            <div className={"Box"}>
+            <h1>This will be a main wraper Box</h1>
+            </div>
+        )
+    }
+}
+
+export default FridgeBox;
