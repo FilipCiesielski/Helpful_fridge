78c6755d8e1d6b4ee7480c030a7b35bb8c54538c
FilipCiesielski <filip.carpenter@gmail.com>
2019-09-10 19:56:57 +0200

    Updates

---
+++ precache-manifest.28c4c64a660d727046f0cd6cb9add090.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b1452018ef8aa2a64168cfcf24d0f2",
+    "url": "/Helpful_fridge/index.html"
+  },
+  {
+    "revision": "530879f894a939381d24",
+    "url": "/Helpful_fridge/static/css/main.889c7666.chunk.css"
+  },
+  {
+    "revision": "c62c9bed3335b29a61fc",
+    "url": "/Helpful_fridge/static/js/2.a07ce42e.chunk.js"
+  },
+  {
+    "revision": "530879f894a939381d24",
+    "url": "/Helpful_fridge/static/js/main.0cb1f4d6.chunk.js"
+  },
+  {
+    "revision": "97dcab95d4983d309ce3",
+    "url": "/Helpful_fridge/static/js/runtime~main.13569a96.js"
+  },
+  {
+    "revision": "1aa5a34524240f38c7a26fc0863e42c4",
+    "url": "/Helpful_fridge/static/media/CL_dyplom_podstawowy_FC-1.1aa5a345.png"
+  },
+  {
+    "revision": "f6593c37dec82085332ec2a899d3f772",
+    "url": "/Helpful_fridge/static/media/Gray Simple Minimalist Resume (2)-1-2_pages-to-jpg-0001.f6593c37.jpg"
+  },
+  {
+    "revision": "1b64b0c2ffdab7380a29cca2c93f19a5",
+    "url": "/Helpful_fridge/static/media/Gray Simple Minimalist Resume (2)-1-2_pages-to-jpg-0002.1b64b0c2.jpg"
+  }
+]);
